e4aed10af6b77d34d03f6dfea68b8d5f9fee1c47
Santiael <rafael.santiago@g.globo>
2025-07-02 22:50:26 -0300

    feat: add reading data from local poke-api submodule

--- app/main.js
+++ app/main.js
@@ -1,5 +1,10 @@
-import { printJsOk } from "transform-data-js";
-import { print_wasm_ok } from "transform-data-wasm";
+import { getPokeApiModule, pokeApiModulesUrl } from "./api.js";
 
-printJsOk();
-print_wasm_ok();
+async function App() {
+  const pokemons = await getPokeApiModule(pokeApiModulesUrl.pokemon);
+  const first150Pokemons = pokemons.slice(0, 150);
+
+  console.table(first150Pokemons, ["id", "name"]);
+}
+
+App();
